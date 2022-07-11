bd00417f5926d7770b17e9a7a5bfda5c933790db
visheshgargavi <vishesh8199@gmail.com>
2022-07-11 10:12:04 +0530

    removed commented part

--- ecr.tf
+++ ecr.tf
@@ -2,7 +2,6 @@
 
 resource "aws_ecr_repository" "aws-ecr" {
   name = "${var.app_name}-${var.app_environment}-ecr"
-  #registory_url = "http://docker.io/ngnix:latest"
   tags = {
     Name        = "${var.app_name}-ecr"
     Environment = var.app_environment
